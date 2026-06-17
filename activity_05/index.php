24b3d2d7e3eade8ee57f5a586aaacb3258acf25e
IraBernal <s2301916@usls.edu.ph>
2026-06-17 13:33:15 +0800

    Add files via upload

---
+++ activity_05/index.php
@@ -0,0 +1,22 @@
+<html> 
+    <head> 
+        <title> Background-Image Practice </title>
+            <link rel="stylesheet" href="css/styles.css"/>
+    </head>
+
+    <body>
+
+    <div class="wrapper">
+        <div class="header">
+
+        </div>
+        <div class="container">
+
+        <div>
+    </div>
+
+    </body>
+
+    <div class="footer">
+    </div>
+</html>
